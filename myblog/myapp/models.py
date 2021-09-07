c2839525be5c221b9f374bddacf7a1935a8ba217
Konstantinos Drosos <dean.drossos@gmail.com>
2021-09-06 23:41:02 -0400

    Fixing models file

--- myblog/myapp/models.py
+++ myblog/myapp/models.py
@@ -6,20 +6,18 @@ from .functionality.projects import Project, ProjectStep
 class ProjectData(models.Model):
 
     """This class contains the data for each project"""
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    owner = models.CharField(max_length=50)
-    members = models.CharField(max_length=500)
-    start_date = models.DateField()
-    deadline = models.DateField()
+    name = models.CharField(max_length=100, default='Next meeting')
+    description = models.CharField(max_length=200, default='Setup next meeting')
+    owner = models.CharField(max_length=50, default='secretary')
+    members = models.CharField(max_length=500, default='Team')
+    start_date = models.DateField(default=('2021', '9', '6'))
+    deadline = models.DateField(default=('2021', '9', '15'))
 
 
 class Profiles(models.Model):
 
     """This class represents a user in the database"""
 
-    def __init__(self, name, email, username) -> None:
-        """
-        Initializes the profile in the database.
-        """
-        self._new_user = UserProfile(name, email, username)
+    name = models.CharField(max_length=50, default='admin')
+    email = models.CharField(max_length=200, default='random@random.com')
+    username = models.CharField(max_length=40, default='admin')
